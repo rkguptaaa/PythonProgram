d7fcf9bdca879ff36e77630273c79d58689299a8
Ravi Gupta <rkguptaaa@gmail.com>
2020-01-10 17:39:27 +0530

    PDF Tutorial

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,5 +1,17 @@
 import PyPDF2
 
-print(dir(PyPDF2))
+class PdfMerger:
+    def __init__(self, path, *args):
+        self.path = path
+        self.filename = args
  
-
+    def pdf_combiner(self):
+        merger = PyPDF2.PdfFileMerger()
+        for file in self.filename:
+            merger.append(file)
+            
+        merger.write('merged.pdf')
+        
+        
+merger = PdfMerger('./', 'twopage.pdf', 'dummy.pdf')
+merger.pdf_combiner()
